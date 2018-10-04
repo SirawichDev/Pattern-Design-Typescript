fdcb553ed573b65b3a3955fceb63ac96d5e12a88
SirawichDev <sirawit0676@gmail.com>
2018-10-04 14:19:45 +0700

    excercise start

--- understand/section1/script.js
+++ understand/section1/script.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 console.log("miew miew");
 var names = 'Ex';
 console.log(names);
@@ -15,3 +26,66 @@ var Color;
 })(Color || (Color = {}));
 var myColor = Color.blue;
 console.log(myColor);
+//any type
+var car = "BMW";
+console.log(car);
+car = { name: "sirawich", numbers: 23 };
+console.log(car);
+//function types
+function n() {
+    return names;
+}
+console.log(n());
+function voider() {
+    console.log('salamut');
+    // return names;
+}
+console.log(voider());
+//argrument type
+function mul(val1, val2) {
+    return val1 * val2;
+}
+console.log(mul(2, 3));
+var vv;
+// let vv:(v:number,v2:number) => number;
+vv = voider;
+vv();
+vv = mul;
+console.log(vv(4, 5));
+//object type
+var userData = {
+    name: "ex",
+    age: 24
+};
+//complex object
+var potter = {
+    num: [2, 4, 5, 6],
+    plus: function (state) {
+        return this.num;
+    }
+};
+console.log(potter.plus(true));
+var potter2 = {
+    num: ["fucck", "fucking"],
+    plus: function (state) {
+        return this.num;
+    }
+};
+console.log(potter2);
+console.log(__assign({}, potter2));
+//union type
+var real = 23;
+real = "freedomm";
+console.log(real);
+//check type
+real = "34";
+if (typeof real == "string") {
+    console.log("booo");
+}
+//never type
+function neverReturn() {
+    throw new Error('ah Error');
+}
+//nullable type
+var canBeNull = 12;
+canBeNull = null;
--- understand/section1/script.ts
+++ understand/section1/script.ts
@@ -18,3 +18,80 @@ enum Color{
 }
 let myColor: Color = Color.blue;
 console.log(myColor);
+
+//any type
+
+let car:any = "BMW";
+console.log(car);
+car = {name:"sirawich",numbers:23};
+console.log(car)
+
+//function types
+function n():string{ //return string type
+    return names;
+}
+console.log(n());
+
+function voider():void{
+    console.log('salamut');
+    // return names;
+}
+console.log(voider());
+//argrument type
+function mul(val1:number,val2:number):number {
+    return val1*val2;
+}
+console.log(mul(2,3));
+
+let vv;
+// let vv:(v:number,v2:number) => number;
+vv = voider;
+vv();
+vv = mul;
+console.log(vv(4,5));
+
+//object type
+let userData:{name:string,age:number} ={
+    name: "ex",
+    age:24
+};
+//complex object
+let potter: {num:number[], plus: (state:boolean) => number[]} = {
+    num: [2,4,5,6],
+    plus: function(state:boolean):number[] {
+        return this.num;
+    }
+}
+
+console.log(potter.plus(true));
+
+//copy type with type alias
+type Complex = {num:string[], plus: (state:boolean) => string[]};
+
+let potter2: Complex ={
+    num: ["fucck","fucking"],
+    plus: function(state:boolean):string[] {
+        return this.num;
+    }
+};
+console.log(potter2);
+console.log({...potter2});
+
+//union type
+let real:number | string = 23;
+real = "freedomm";
+console.log(real);
+
+//check type
+real = "34"
+if(typeof real == "string"){
+    console.log("booo");
+}
+//never type
+function neverReturn():never {
+    throw new Error('ah Error');
+} 
+
+//nullable type
+let canBeNull = 12;
+canBeNull =null;
