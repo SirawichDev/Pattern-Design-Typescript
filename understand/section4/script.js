8e8313e1e483269ba9e582c6a66e64dd0390935d
SirawichDev <sirawit0676@gmail.com>
2018-10-05 14:45:44 +0700

    inheritance

--- understand/section4/script.js
+++ understand/section4/script.js
@@ -1,9 +1,45 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.age = 23; //access any class extenn Person class
         this.name = name;
     }
+    //Method
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+        this.setType("foo");
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
 var person = new Person("Ex", "Ex007");
 console.log(person.name, person.username);
+person.printAge();
+// person.setType("sds"); won't work with private method
+//Inheritance
+var Ex = /** @class */ (function (_super) {
+    __extends(Ex, _super);
+    // name = 'xx';
+    function Ex(username) {
+        return _super.call(this, "Max", username) || this;
+    }
+    return Ex;
+}(Person));
+var exs = new Ex("adn"); //อิงจาก constructor ตัว Person
+console.log(exs);
--- understand/section4/script.ts
+++ understand/section4/script.ts
@@ -1,12 +1,35 @@
 class Person {
     name: string;
     private type:string; //only access obj you create
-    protected number:number;  //access any class extenn Person class
+    protected age:number = 23;  //access any class extenn Person class
 
     constructor(name:string,public username:string){ //argrument of Person
         this.name =name;
+    }
+    //Method
+    printAge(){
+        console.log(this.age);
+        this.setType("foo");
+    }
+    private setType(type:string){
+        this.type = type;
+        console.log(this.type);
     }
 }
 
 const person = new Person("Ex","Ex007");
 console.log(person.name,person.username);
+person.printAge();
+// person.setType("sds"); won't work with private method
+
+
+//Inheritance
+class Ex extends Person{
+   // name = 'xx';
+    constructor(username:string){ 
+        super("Max",username); //ส่งไปยัง constructure ของ class แม่
+    }
+}
+
+const exs = new Ex("adn");//อิงจาก constructor ตัว Person
+console.log(exs);
