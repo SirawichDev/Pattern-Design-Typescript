c7fee8167f5d08e11121e0f90f20c7f67ad940e7
SirawichDev <sirawit0676@gmail.com>
2018-10-10 12:09:44 +0700

    interface and function

--- understand/section6/scripts.js
+++ understand/section6/scripts.js
@@ -5,10 +5,31 @@ function hello(person) {
 var person = {
     firstname: "sirawich",
     age: 23,
-    none: 23,
     greet: function (name) {
         console.log("Hello ", this.firstname, name);
     }
 };
 hello(person);
 person.greet("Ex");
+//Class
+var Miew = /** @class */ (function () {
+    function Miew() {
+        this.age = 20;
+    }
+    Miew.prototype.greet = function (name) {
+        console.log(this.firstnames);
+    };
+    return Miew;
+}());
+function ttest(cat) {
+    console.log(cat.firstnames);
+}
+var cat = new Miew();
+cat.firstnames = "Sirawich";
+ttest(cat); //เรียก hello
+cat.greet("sry sry");
+var myDoubleFunc;
+myDoubleFunc = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(myDoubleFunc(20, 30));
--- understand/section6/scripts.ts
+++ understand/section6/scripts.ts
@@ -6,16 +6,43 @@ interface NamePerson {
 }
 function hello(person:NamePerson){
     console.log(person.firstname + ' ' + person.age);
-
 }
 const person = {
     firstname:"sirawich",
     age:23,
-    none: 23,
     greet(name:string){
         console.log("Hello " , this.firstname, name);
     }
 };
 hello(person);
 person.greet("Ex");
+interface Test {
+    firstnames:string;
+    age?: number;
+}
+//Class
+class Miew implements Test{
+    firstnames:string;
+    age:number = 20;
+    greet(name:string){
+        console.log(this.firstnames);
+    }
+}
+function ttest(cat:Test){
+    console.log(cat.firstnames)
+}
+let cat = new Miew();
+cat.firstnames = "Sirawich";
+ttest(cat); //เรียก hello
+cat.greet("sry sry");
 
+//function type
+interface DoubleValueFunc {
+    (number1:number,number2:number):number; //function
+}
+
+let myDoubleFunc: DoubleValueFunc;
+myDoubleFunc= function (n1:number,n2:number) {
+    return n1+n2;
+};
+console.log(myDoubleFunc(20,30));
