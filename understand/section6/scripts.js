75f53cc5b4cce027231dc6e2b9935c62a2a837b2
SirawichDev <sirawit0676@gmail.com>
2018-10-10 10:57:58 +0700

    method in interface

--- understand/section6/scripts.js
+++ understand/section6/scripts.js
@@ -1,9 +1,14 @@
 "use strict";
 function hello(person) {
-    console.log(person.firstname);
+    console.log(person.firstname + ' ' + person.age);
 }
 var person = {
-    firstname: "sd",
-    age: 20
+    firstname: "sirawich",
+    age: 23,
+    none: 23,
+    greet: function (name) {
+        console.log("Hello ", this.firstname, name);
+    }
 };
 hello(person);
+person.greet("Ex");
--- understand/section6/scripts.ts
+++ understand/section6/scripts.ts
@@ -1,11 +1,21 @@
 interface NamePerson {
     firstname: string;
+    age?: number; //not fix it fine if doesn't have age pass to interface
+    [unknow : string]: any; // not array ใช้ต่อเมื่อไม่ทราบชื่อ properties ที่ส่งมา เจ้านี้จะเป็น properties นั้นให้ <ปกติ ชื่อ prop ต้องตรงกันกับ interface>
+    greet(name:string):void; //method
 }
 function hello(person:NamePerson){
-    console.log(person.firstname);
+    console.log(person.firstname + ' ' + person.age);
+
 }
 const person = {
-    firstname:"sd",
-    age:20
+    firstname:"sirawich",
+    age:23,
+    none: 23,
+    greet(name:string){
+        console.log("Hello " , this.firstname, name);
+    }
 };
 hello(person);
+person.greet("Ex");
+
