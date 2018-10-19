629c450869cab1c5729511785187e43afe758b0e
SirawichDev <sirawit0676@gmail.com>
2018-10-19 22:07:06 +0700

    Generic Array

--- understand/section7/script.js
+++ understand/section7/script.js
@@ -2,13 +2,33 @@
 function echo(data) {
     return data;
 }
-console.log(echo("Max"));
+//Generic ช่วยบอก compiler ให้ว่า type นี้มี propertie หรือชนิดเป็นอะไรได้บา้ง
+console.log(echo("Ex"));
 console.log(echo(27));
 console.log(echo({ name: "Max", age: 23 }));
-//better Generic
+//better Generic ช่วยให้ ใช้ properties ของ parameter  type ได้ง่ายขึ้น
 function Better(data) {
     return data;
 }
-console.log(Better("Max").length);
+console.log(Better("Ex").length); //Generic แสดง properties ที่มีให้เฉพาะ type ของ parameterเทานั้น
 console.log(Better(27));
-console.log(Better({ name: "Max", age: 23 }));
+// console.log(Better<number>("27")); //error 
+console.log(Better({ name: "Ex", age: 23 }));
+//Buit-in Generics 
+var testResult = [2, 4, 5]; //กำหนด ว่า typeอะไรบ้างที่สามารถอยู๋ได้ใน  ที่Array ควรจะคลุมไว้
+testResult.push(3);
+// testResult.push("343"); //error
+console.log(testResult);
+// Arrays
+function print(arg) {
+    arg.forEach(function (element) { return console.log(element); });
+}
+print(["tt", "tt"]);
+function print(arg, x) {
+    arg.forEach(function (element) { return console.log(element); });
+    console.log(x);
+}
+print(["x", "y"], 2); //บอกว่า ค่าที่ส่งไปยังฟังค์ชันมีtypeอะไร
+// print<number>(["x","y"],2); //error
+// print<string>(["x","y"],2); //error
+// print<number>(["x","y"]); //error
--- understand/section7/script.ts
+++ understand/section7/script.ts
@@ -2,15 +2,37 @@
 function echo(data:any){
     return data;
 }
+//Generic ช่วยบอก compiler ให้ว่า type นี้มี propertie หรือชนิดเป็นอะไรได้บา้ง
 
-console.log(echo("Max"));
+console.log(echo("Ex"));
 console.log(echo(27));
 console.log(echo({name: "Max", age: 23}));
 
-//better Generic
-function Better<T>(data : T){
+//better Generic ช่วยให้ ใช้ properties ของ parameter  type ได้ง่ายขึ้น
+function Better<T>(data : T){ //T คือ type ของ parameter นั้น 
     return data;
 }
-console.log(Better("Max").length);
+console.log(Better("Ex").length); //Generic แสดง properties ที่มีให้เฉพาะ type ของ parameterเทานั้น
 console.log(Better(27));
-console.log(Better({name: "Max", age: 23}));
+// console.log(Better<number>("27")); //error 
+console.log(Better({name: "Ex", age: 23}));
+
+//Buit-in Generics 
+const testResult:Array<number> = [2,4,5]; //กำหนด ว่า typeอะไรบ้างที่สามารถอยู๋ได้ใน  ที่Array ควรจะคลุมไว้
+testResult.push(3);
+// testResult.push("343"); //error
+console.log(testResult);
+
+// Arrays
+function print<T>(arg: T[]){ //parameter ที่ส่งมาจะมีtypeอะไรก็ได้แต่ต้องมีarray คลุ่มไว้
+    arg.forEach((element) => console.log(element));
+}
+print<string>(["tt","tt"]);
+function print<T>(arg: T[],x: T){ //parameter ที่ส่งมาจะมีtypeอะไรก็ได้แต่ต้องมีarray คลุ่มไว้
+    arg.forEach((element) => console.log(element));
+    console.log(x);
+}
+print<number|string>(["x","y"],2); //บอกว่า ค่าที่ส่งไปยังฟังค์ชันมีtypeอะไร
+// print<number>(["x","y"],2); //error
+// print<string>(["x","y"],2); //error
+// print<number>(["x","y"]); //error
